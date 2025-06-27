39ab062dc4ff82923b1c6ce22b2e2fec506c0831
Maryamabdi52 <maryamabdimohamed63@gmail.com>
2025-06-28 00:39:48 +0700

    add gdown download for model

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import AutoTokenizer, TFAutoModelForSeq2SeqLM
+import os
+import gdown
 
 app = FastAPI()
 
@@ -8,6 +10,15 @@ app = FastAPI()
 model_dir = "qalpi_model"  # ama path-ka saxda ah ee model-kaaga
 tokenizer = AutoTokenizer.from_pretrained(model_dir)
 model = TFAutoModelForSeq2SeqLM.from_pretrained(model_dir)
+
+# Hubi in folder-ka model-ka uu jiro
+os.makedirs("qalpi_model", exist_ok=True)
+
+file_id = "1T0mt42_0145xSAY2WULB9JE9kOqA-aCX"
+destination = "qalpi_model/tf_model.h5"
+if not os.path.exists(destination):
+    url = f"https://drive.google.com/uc?id={file_id}"
+    gdown.download(url, destination, quiet=False)
 
 class TranslateRequest(BaseModel):
     text: str
